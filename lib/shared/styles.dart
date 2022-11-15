2c076d37f0b13001b2ec8b08240f6a720bc91b6e
ahmadm712 <ahmadmuji2808@gmail.com>
2022-11-15 16:00:53 +0700

    create styles

---
+++ lib/shared/styles.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color kPrimaryColor = Color(0xff2B637B);
+const Color kBlackColor = Colors.black;
+const Color kStrongBlueColor = Color(0xff554AF0);
+const Color kGreyColor = Color(0xff686777);
+
+const Color kLightBackgroundColor = Color(0xffF6F8FB);
+const Color kDarkBackgroundColor = Color(0xff020518);
+const Color kNumberBackgroundColor = Color(0xff1A1D2E);
+
+// Text Styles
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: Colors.white,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+
+// Font Weight
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
