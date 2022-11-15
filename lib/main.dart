0e0f0180d65d34afd247e9bb85e8cfb43854e427
ahmadm712 <ahmadmuji2808@gmail.com>
2022-11-15 16:02:59 +0700

    finish up the test

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:test_suitmedia/cubits/list_users_cubit/list_users_cubit.dart';
 import 'package:test_suitmedia/pages/home_page.dart';
+import 'package:test_suitmedia/services/api_services.dart';
+import 'package:test_suitmedia/shared/styles.dart';
+import 'package:test_suitmedia/utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +16,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Test Suitmedia',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+    final ApiServices apiServices = ApiServices();
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => ListUsersCubit(apiServices)..fetchUsers(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Test Suitmedia',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.deepOrange,
+          inputDecorationTheme: const InputDecorationTheme(
+            filled: true,
+            fillColor: Colors.white,
+          ),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.white,
+            elevation: 0.5,
+            centerTitle: true,
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kPrimaryColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              textStyle: whiteTextStyle.copyWith(fontSize: 14),
+            ),
+          ),
+        ),
+        home: const HomePage(),
+        onGenerateRoute: mainRoutes,
       ),
-      home: const HomePage(),
     );
   }
 }
